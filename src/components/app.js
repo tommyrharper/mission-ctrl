345d38771b3168590f1d5fc0bd968d5b6d86c106
Alex Lewis <alexlewis374@gmail.com>
2020-05-27 15:12:51 +0100

    Test - check initial setup

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    )
+  }
+}
+
+export default App
