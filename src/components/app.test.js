76c7da5d703606819383cb459f342d3dd3fe7747
Alex Lewis <alexlewis374@gmail.com>
2020-05-27 16:03:25 +0100

    Install jest-ezyme. Test - hello world is on the page

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import App from './app.js';
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16.1'
 
 
-configure({ adapter: new Adapter() });
+// configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   shallow(<App />);
 });
+
+describe('Hello world appears', () => {
+  it('hello world is rendered', () => {
+    const wrapper = shallow(<App />)
+    const html = <div><p>Hello World</p></div>
+    expect(wrapper).toContainReact(html);
+  })
+})
---
+++ src/setupTests.js
@@ -0,0 +1,7 @@
+// for enzyme
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import "jest-enzyme";
+configure({ adapter: new Adapter() });
+// for rtl
+import "@testing-library/jest-dom/extend-expect";
