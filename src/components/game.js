1c0a05fe75036ef849138a147a253ae46a3f5c8d
tommyrharper <tommyrharper@gmail.com>
2020-05-28 10:50:17 +0100

    Update Game component to have multiple levels

--- src/components/game.js
+++ src/components/game.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
 export class Game extends Component {
+  constructor() {
+    super()
+      this.state = {
+        level: 1
+      }
+  }
+  correctAnswer = () => {
+    let nextLevel = this.state.level + 1
+    this.setState({level: nextLevel})
+  }
+  wrongAnswer = () => {
+    alert("WRONG!")
+  }
   render() {
+    let level
+    if (this.state.level === 1) {
+      level = <p>Copy</p>
+    } else if (this.state.level === 2) {
+      level = <p>Paste</p>
+    } else if (this.state.level === 3) {
+      level = <p>Cut</p>
+    } else {
+      level = <p>Game Complete</p>
+    }
     return (
       <div>
         <p>Welcome to the Game</p>
-        <p>Copy</p>
+        {level}
+        <button onClick={this.correctAnswer}>Correct answer</button>
+        <button onClick={this.wrongAnswer}>Wrong answer</button>
       </div>
     )
   }
