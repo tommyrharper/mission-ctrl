961b38ac0b796028ed91fcb2d1cba653114c38e7
Harry Turnbull <hturnbull@live.co.uk>
2020-05-28 16:12:48 +0100

    Game uses Questions

--- src/components/game.js
+++ src/components/game.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react'
+import Question from './question'
 
 export class Game extends Component {
   constructor() {
     super()
-      this.state = {
-        level: 1,
-        warning: "",
-        numberFailures: 0
-      }
-  }
-  correctAnswer = () => {
-    let nextLevel = this.state.level + 1
-    this.setState({
-      level: nextLevel,
-      warning: "",
-      numberFailures: 0
-    })
-  }
-  wrongAnswer = () => {
-    let numberFailures = this.state.numberFailures + 1
-    this.setState({
-      warning: "Wrong",
-      numberFailures: numberFailures
-    })
-  }
-  render() {
-    let warning = this.state.warning
-    let level
-    switch (this.state.level) {
-      case 1:
-        level = <p>Copy</p>
-        break;
-      case 2:
-        level = <p>Paste</p>
-        break;
-      case 3:
-        level = <p>Cut</p>
-        break;
-      default:
-        level = <p>Game Complete</p>
+    this.state = {
+      level: 0,
+      totalErrors: 0,
+      totalCorrect: 0,
+      shortcutSet: [
+        { name: "Copy", combo: ['Control', 'c'] },
+        { name: "Cut", combo: ['Control', 'x'] },
+        { name: "Undo", combo: ['Control', 'z'] },
+        { name: "Paste", combo: ['Control', 'v'] },
+      ],
+      currentShortcut: 0
     }
+  }
+
+  attempt = (correct) => {
+    if (correct) {
+      this.setState({
+        totalCorrect: this.state.totalCorrect + 1,
+        currentShortcut: this.randomShortcut()
+      })
+    } else {
+      this.setState({
+        totalErrors: this.state.totalErrors + 1
+      })
+    }
+  }
+
+  render() {
+
     return (
       <div>
         <p>Welcome to the Game</p>
-        {level}
-        <p>{warning}</p>
-        <button onClick={this.correctAnswer}>Correct answer</button>
-        <button onClick={this.wrongAnswer}>Wrong answer</button>
+        <p>Total Errors: {this.state.totalErrors}</p>
+        <p>Total Correct: {this.state.totalCorrect}</p>
+        <p>Level: {this.state.level}</p>
+        <Question 
+          shortcut={this.state.shortcutSet[this.state.currentShortcut]}
+          attempt={this.attempt}
+        />
       </div>
     )
+  }
+
+  randomShortcut = () => {
+    const shortcutSet = this.state.shortcutSet
+    return Math.floor(Math.random() * shortcutSet.length)
   }
 }
 
