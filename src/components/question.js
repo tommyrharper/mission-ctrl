cc51c0835e59484a6589429b7939359a8284e4c8
Harry Turnbull <hturnbull@live.co.uk>
2020-05-28 16:12:33 +0100

    add question component

---
+++ src/components/question.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export class Question extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentKeys: []
+    }
+  }
+
+  componentDidMount() {
+    this.keyDownListener()
+    this.keyUpListener()
+  }
+
+  keyDownListener() {
+    document.addEventListener('keydown', (e) => {
+      e.preventDefault()
+      if (!e.repeat) {
+        const newKeys = [...this.state.currentKeys]
+        newKeys.push(e.key)
+        this.setState({
+          currentKeys: newKeys
+        })
+        console.log('newKeys', newKeys)
+        if (newKeys.length === this.props.shortcut.combo.length) {
+          if (this.compareArrays(newKeys, this.props.shortcut.combo)) {
+            this.props.attempt(true)
+          }
+          else {
+            this.props.attempt(false)
+          }
+        }
+      }
+    })
+  }
+
+  keyUpListener() {
+    document.addEventListener('keyup', (e) => {
+      this.setState({
+        currentKeys: []
+      })
+    })
+  }
+
+  render() {
+    return (
+      <h2>{this.props.shortcut.name}</h2>
+    )
+  }
+
+  compareArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false
+
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false
+    }
+    return true
+  }
+}
+
+export default Question
