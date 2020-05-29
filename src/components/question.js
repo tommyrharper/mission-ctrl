d4ddf53dfd18e782593acfbef19a1a9a543e5383
tommyrharper <tommyrharper@gmail.com>
2020-05-29 15:00:42 +0100

    Remove console.log statements

--- src/components/question.js
+++ src/components/question.js
@@ -15,10 +15,8 @@ export class Question extends Component {
 
   keyDownListener() {
     document.addEventListener('keydown', (e) => {
-      console.log('event listener triggered')
       e.preventDefault()
       if (!e.repeat) {
-        console.log("inside !e.repeat statement")
         const newKeys = [...this.state.currentKeys]
         newKeys.push(e.key)
         this.setState({
@@ -40,7 +38,6 @@ export class Question extends Component {
 
   keyUpListener() {
     document.addEventListener('keyup', (e) => {
-      console.log("inside keyUpL event listener")
       this.setState({
         currentKeys: []
       })
@@ -48,7 +45,6 @@ export class Question extends Component {
   }
 
   render() {
-    console.log(this.state.currentKeys,'this.state.currentKeys')
     return (
       <div>
       <h2>{this.props.shortcut.name}</h2>
