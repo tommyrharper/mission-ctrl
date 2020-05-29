daa460df2f55924557f6aafc94b082dab8b49028
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-29 14:50:27 +0100

    Add: mistakes score to gameComplete component

--- src/components/gameComplete.test.js
+++ src/components/gameComplete.test.js
@@ -6,16 +6,22 @@ it('renders without crashing', () => {
   shallow(<GameComplete />)
 })
 
-it('it says game complete', () => {
+it(' says game complete', () => {
   const wrapper = shallow(<GameComplete />)
   const message = <p>Game Complete</p>
   expect(wrapper).toContainReact(message)
 })
 
-it('it says score', () => {
+it('says score', () => {
   const wrapper = shallow(<GameComplete score={7} mistakes={2} />)
   const score = <p>Total Correct: 7</p>
   expect(wrapper).toContainReact(score)
+})
+
+it('says mistakes',() => {
+  const wrapper = shallow(<GameComplete score={7} mistakes={2} />)
+  const mistakes = <p>Total Mistakes: 2</p>
+  expect(wrapper).toContainReact(mistakes)
 })
 // score 
 // mistakes 
