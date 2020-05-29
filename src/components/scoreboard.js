4d7b3fe0658f525c6a40c7bd2d612e4ee062c73c
Graham <grahamalconer@icloud.com>
2020-05-29 16:24:30 +0100

    Styled by Alex and Graham

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -7,51 +7,24 @@ export class Scoreboard extends Component {
       scores: [{"name": "Alex", "date":"29/05/20", "score": 10000 }, {"name": "Graham", "date":"29/05/20", "score": 20000 }]
     }
   }
-  // const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
-  // makeScoreboard = (array) => {
-  //   array.forEach()
-  // }
-   
-    // makeScoreboard = () => {
-    //   let array = this.state.scores
-    //   for (let i = 0; i < array.length; i++) {
-    //     return <li>{this.state.scores[i][0]}, {this.state.scores[i][1]}, {this.state.scores[i][2]}</li>
-    //   }
-    // }
-
 
   render() {
-    let listedScores = this.state.scores
+    let unsortedScores = this.state.scores
+    let sortedScores = unsortedScores.sort(function (a, b) {
+    return b.score - a.score
+    })
 
+    const scoresPrint = sortedScores.map((score) => <li key={score.name}>{score.name} {score.date} {score.score}</li>)
 
-  let sortedScores = listedScores.sort(function (a, b) {
-  return b.score - a.score;
-});
-
-
-
-
-  const scoresPrint = sortedScores.map((score) => <li key={score.name}>{score.name} {score.date} {score.score}</li>)
- 
     return (
       <div>
         <h2>Scoreboard</h2>
         <ul>
           {scoresPrint}
-
-        {/* <li>{this.state.scores[i][0]}, {this.state.scores[i][1]}, {this.state.scores[i][2]}</li> */}
-
         </ul>
       </div>
     )
   }
 }
-
-// var fruits = ["apple", "orange", "cherry"];
-// fruits.forEach(myFunction);
-
-// function myFunction(item, index) {
-//   document.getElementById("demo").innerHTML += index + ":" + item + "<br>"; 
-// }
 
 export default Scoreboard
