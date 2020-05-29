a90c6a11a5105548a14cc1659b23ea8f4b6736b7
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-29 14:45:50 +0100

    Add: correct score to gameComplete component

--- src/components/gameComplete.test.js
+++ src/components/gameComplete.test.js
@@ -11,3 +11,12 @@ it('it says game complete', () => {
   const message = <p>Game Complete</p>
   expect(wrapper).toContainReact(message)
 })
+
+it('it says score', () => {
+  const wrapper = shallow(<GameComplete score={7} mistakes={2} />)
+  const score = <p>Total Correct: 7</p>
+  expect(wrapper).toContainReact(score)
+})
+// score 
+// mistakes 
+
