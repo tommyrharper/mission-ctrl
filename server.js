8ec2064e05da4f0f6c2f4be16e687a3df63873c6
Harry Turnbull <hturnbull@live.co.uk>
2020-05-29 12:36:47 +0100

    add some clarifying comments

--- server.js
+++ server.js
@@ -6,24 +6,28 @@ const app = express();
 
 app.use(express.json());
 
-//DB Config
-mongoose.connect(
-  process.env.dbURI,
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  () => console.log("connected to db")
-)
-.catch(error => console.log(error))
+// DB Connection
+mongoose
+  .connect(
+    process.env.dbURI,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => console.log("connected to db")
+  )
+  .catch((error) => console.log(error));
 
-const port = process.env.PORT || 5000
+// Port assignment
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send("Hello World")
-})
+// Server listen
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.post('/', (req, res) => {
-  res.send("You posted")
-})
+app.post("/", (req, res) => {
+  res.send("You posted");
+});
 
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+// Server listen
+app.listen(port, () => console.log(`Server listening on port ${port}`));
 
-module.exports = app
+module.exports = app;
