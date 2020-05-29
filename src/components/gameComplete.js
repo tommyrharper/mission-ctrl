936bc8eec330e7e658ade069f4a5a0011fe9c353
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-29 14:45:50 +0100

    Add: correct score to gameComplete component

--- src/components/gameComplete.js
+++ src/components/gameComplete.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 export class GameComplete extends Component {
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
       <div>
         <p>Game Complete</p>
+        <p>Total Correct: {this.props.score}</p>
       </div>
     )
   }
 }
+
+
 
 export default GameComplete
