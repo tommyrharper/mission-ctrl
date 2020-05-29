9c14ad711c3f3d01fd7936ec861515185edf2cfe
tommyrharper <tommyrharper@gmail.com>
2020-05-29 11:59:44 +0100

    Test for message 'Game Complete'

--- src/components/gameComplete.test.js
+++ src/components/gameComplete.test.js
@@ -6,3 +6,8 @@ it('renders without crashing', () => {
   shallow(<GameComplete />)
 })
 
+it('it says game complete', () => {
+  const wrapper = shallow(<GameComplete />)
+  const message = <p>Game Complete</p>
+  expect(wrapper).toContainReact(message)
+})
