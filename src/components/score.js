251a3d66fb455dd38167e2f1ca41ffb722263de4
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-30 12:43:19 +0100

    Add: connected game component to child Score component

--- src/components/score.js
+++ src/components/score.js
@@ -10,35 +10,35 @@ export class Score extends Component {
   }
 
   static getDerivedStateFromProps(props, state) {
-    
+    //This stores the failures of this turn in the state
     if (props.failuresThisTurn > 0){
       return{
         failuresLastTurn: props.failuresThisTurn
       }
     }
 
-// evaulating if correct 
+    // Add no points if player failed 3 or more times
     if (props.failuresThisTurn === 0 && state.failuresLastTurn >= 3) {
       return {
         score: state.score + 0,
         failuresLastTurn: props.failuresThisTurn
       };
-    }
-
+    } 
+    // Adds 1 point if player failed 2 times
     if (props.failuresThisTurn === 0 && state.failuresLastTurn === 2) {
       return {
         score: state.score + 1,
         failuresLastTurn: props.failuresThisTurn
       };
     }
-
+    // Adds 3 points if player failed 1 timee
     if (props.failuresThisTurn === 0 && state.failuresLastTurn === 1) {
       return {
         score: state.score + 3,
         failuresLastTurn: props.failuresThisTurn
       };
     }
-    
+    // Adds 5 points if player got it right first time
     if (props.failuresThisTurn === 0 && props.numberOfCorrect > 0) {
       return {
         score: state.score + 5,
