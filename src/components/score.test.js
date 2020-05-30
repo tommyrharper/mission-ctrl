362f1459669682eb52d6dc8f2f3e7a6060df1040
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-30 10:45:15 +0100

    gives  a score of 5 when score straight away

--- src/components/score.test.js
+++ src/components/score.test.js
@@ -7,6 +7,16 @@ it('renders without crashing', () => {
 })
 
 it('gives a score of 0 on load', () => {
-  const wrapper = shallow(<Score numberOfFailures={0}/>)
+  const wrapper = shallow(<Score totalFailures={0} failuresThisTurn={0} numberOfCorrect={0}/> )
   expect(wrapper).toContainReact(<p>Score: 0</p>)
+})
+
+it('gives  a score of 5 when score straight away',() => {
+  const wrapper  = shallow (<Score totalFailures={0} failuresThisTurn={0} numberOfCorrect={1}/> )
+  expect(wrapper).toContainReact(<p>Score: 5</p>)
+})
+
+it('gives a score of 10 with two first time correct answers', () => {
+  const wrapper  = shallow (<Score totalFailures={0} failuresThisTurn={0} numberOfCorrect={2}/> )
+  expect(wrapper).toContainReact(<p>Score: 10</p>)
 })
