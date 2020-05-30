8e9e77de6c7f0e688c7ba402b7f87b680a1fe852
Harry Turnbull <hturnbull@live.co.uk>
2020-05-30 15:03:31 +0100

    separate client

--- client/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-// for enzyme
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
-// for rtl
-import '@testing-library/jest-dom/extend-expect'
-configure({ adapter: new Adapter() })
