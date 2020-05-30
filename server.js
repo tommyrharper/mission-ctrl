53a74df0da688a5ae8ec9f017f119a2e00574508
Harry Turnbull <hturnbull@live.co.uk>
2020-05-30 10:25:54 +0100

    change name of env var

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use('/scores', scoresRoute);
 // DB Connection
 mongoose
   .connect(
-    process.env.dbURI,
+    process.env.DB_URI,
     { useUnifiedTopology: true, useNewUrlParser: true },
     () => console.log("connected to db")
   )
