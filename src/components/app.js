96ce6d451f2ccb3f584a409f54312013b75bc134
tommyrharper <tommyrharper@gmail.com>
2020-06-01 19:07:15 +0100

    Add requestQuestions() and formatJSON() in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -16,12 +16,57 @@ export class App extends Component {
   };
 
   chooseWindows = () => {
+    this.requestQuestions()
     this.setState({ operatingSystem: "windows" })
   }
 
   chooseMac = () => {
+    this.requestQuestions()
     this.setState({ operatingSystem: "mac" })
   }
+
+  componentDidMount = () => {
+    this.requestQuestions()
+  }
+
+  requestQuestions = () => {
+    fetch('../../questions.json', {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.formatJSON(result)
+        },
+      )
+  }
+
+  formatJSON = (questionsJson) => {
+    let nameList
+    let comboList
+    if (this.state.operatingSystem === 'mac') {
+      console.log("props is  mac")
+      nameList = Object.keys(questionsJson.mac)
+      comboList = Object.values(questionsJson.mac)
+    } else {
+      console.log("props is  windows")
+      nameList = Object.keys(questionsJson.windows)
+      comboList = Object.values(questionsJson.windows)
+    }
+
+    let shortcutSet = []
+    let question
+    let a = 0
+    nameList.forEach(function(name) {
+      question = { name: name, combo: comboList[a]}
+      shortcutSet.push(question)
+      a += 1
+    })
+    this.setState({shortcutSet: shortcutSet})
+    // console.log(this.state.shortcutSet)
+  }
+
 
   render() {
     let game
@@ -29,17 +74,17 @@ export class App extends Component {
     let scoreboard
     let toggle
     if (this.state.show) {
-      game = <Game operatingSystem={this.state.operatingSystem} />
+      game = <Game shortcutSet={this.state.shortcutSet} />
     } else {
       toggle = <div>
                 <p>Please select your operating system:</p>
                 <input type="radio" id="mac" name="OS" value="mac" onClick={this.chooseMac}/>
-                <label for="mac">Mac</label>
+                <label htmlFor="mac">Mac</label>
                 <input type="radio" id="windows" name="OS" value="windows" onClick={this.chooseWindows}/>
-                <label for="female">Windows</label>
+                <label htmlFor="female">Windows</label>
                </div>
       button = <button onClick={this.startGame}>Start</button>
-      scoreboard = <Scoreboard />
+      // scoreboard = <Scoreboard />
     }
     return (
       <div>
