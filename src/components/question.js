4b46f22eb6a1fb2d5e1d2e857089c6746b0c67c9
Harry Turnbull <hturnbull@live.co.uk>
2020-06-01 11:58:18 +0100

    RF: extract handleIncorrect

--- src/components/question.js
+++ src/components/question.js
@@ -32,12 +32,7 @@ export class Question extends Component {
         if (this.compareArrays(newKeys, this.props.shortcut.combo)) {
           this.props.attempt(this.state.score, this.state.incorrectAttempts);
         } else {
-          let newScore = this.state.score - 2
-          if (newScore < 0) newScore = 0
-          this.setState({
-            incorrectAttempts: this.state.incorrectAttempts + 1,
-            score: newScore
-          })
+          this.handleIncorrect();
         }
       }
     }
@@ -49,6 +44,16 @@ export class Question extends Component {
       currentKeys: [],
     });
   };
+
+  handleIncorrect() {
+    let newScore = this.state.score - 2;
+    if (newScore < 0)
+      newScore = 0;
+    this.setState({
+      incorrectAttempts: this.state.incorrectAttempts + 1,
+      score: newScore
+    });
+  }
 
   render() {
     return (
