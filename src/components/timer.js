3f40138887788f29ef3c6c7fc0b0991bce82e4b8
Alexa <alexakearns@outlook.com>
2020-06-01 15:59:29 +0100

    timer stops at zero and called timesUp

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // import parent eventually 
 
-export  class Timer extends Component {
+export class Timer extends Component {
   constructor(props){
     super(props)
     this.state = {
@@ -9,10 +9,26 @@ export  class Timer extends Component {
     }
   }
 
-  
+  componentDidMount() {
+    this.secInterval = setInterval(() => {
+      if (this.state.seconds > 0) {
+        this.setState(prevState => ({
+          seconds: prevState.seconds -1
+        }))
+      } 
+      if (this.state.seconds === 0) {
+        clearInterval(this.secInterval)
+        this.timesUp()
+      }
+    },1000)
+  } 
+
+  timesUp = () => {
+    console.log("times Up")
+  }
 
   render() {
-    let seconds = <p>Time left: {this.state.seconds}</p>
+    const seconds = <p>Time left: {this.state.seconds}</p>
     return (
       <div>
         {seconds}
