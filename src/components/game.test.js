dc0eb08acbe5aefc7ffe06049c7aeda20a2a37a1
tommyrharper <tommyrharper@gmail.com>
2020-06-01 19:39:00 +0100

    Update tests to use the new game component

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Game from './game.js'
 import { shallow } from 'enzyme'
 
+const shortcutSet = [
+  { name: "Copy", combo: ['Control', 'c'] },
+  { name: "Cut", combo: ['Control', 'x'] },
+  { name: "Undo", combo: ['Control', 'z'] },
+  { name: "Paste", combo: ['Control', 'v'] },
+]
+
 it('renders without crashing', () => {
-  shallow(<Game />)
+  shallow(<Game shortcutSet={shortcutSet} />)
 })
 
 
 it('it welcomes the user', () => {
-  const wrapper = shallow(<Game />)
+  const wrapper = shallow(<Game shortcutSet={shortcutSet}/>)
   const welcome = <h1>Mission-Ctrl</h1>
   expect(wrapper).toContainReact(welcome)
 })
