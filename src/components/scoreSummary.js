219e1223c7895bc0db97f1093b342d1ccef6984a
tommyrharper <tommyrharper@gmail.com>
2020-06-01 12:21:15 +0100

    Extract calculate to a method

--- src/components/scoreSummary.js
+++ src/components/scoreSummary.js
@@ -4,16 +4,20 @@ export class ScoreSummary extends Component {
   constructor(props){
     super(props)
   }
+  calculateAccuracy = () => {
+    return (this.props.totalCorrect / (this.props.totalCorrect + this.props.totalIncorrect)) * 100
+  }
   render() {
+    let accuracy = this.calculateAccuracy()
     return (
       <div>
         <h2>Score Summary</h2>
-        <li>Your score: {this.props.score}</li>
-        <li>Total Correct: {this.props.totalCorrect}</li>
-        <li>Total Incorrect: {this.props.totalIncorrect}</li>
-        <li>Round Accuracy: {
-        (this.props.totalCorrect / (this.props.totalCorrect + this.props.totalIncorrect)) * 100}%
-        </li>
+        <ul>
+          <li>Your score: {this.props.score}</li>
+          <li>Total Correct: {this.props.totalCorrect}</li>
+          <li>Total Incorrect: {this.props.totalIncorrect}</li>
+          <li>Round Accuracy: {accuracy}%</li>
+        </ul>
       </div>
     )
   }
