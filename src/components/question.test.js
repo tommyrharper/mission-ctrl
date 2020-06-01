a1b550484e18deef32c60a44bd21d4736883068f
Harry Turnbull <hturnbull@live.co.uk>
2020-06-01 10:48:59 +0100

    R: correct calls attempt with score 5, incorrect 0

--- src/components/question.test.js
+++ src/components/question.test.js
@@ -1,27 +1,43 @@
+import React from "react";
+import Question from "./question.js";
+import { shallow, mount } from "enzyme";
 
-import React from 'react'
-import Question from './question.js'
-import { shallow, mount } from 'enzyme'
-
-it('renders without crashing', () => {
+it("renders without crashing, taking a shortcut to render and attempt method prop", () => {
   let mockAttempt = {
-    attempt: function () {}
-  }
-  const mockShortcut = { name: "Copy", combo: ['c'] }
-  shallow(<Question shortcut={mockShortcut} attempt={mockAttempt.attempt}/>)
-})
+    attempt: function () {},
+  };
+  const mockShortcut = { name: "Copy", combo: ["c"] };
+  shallow(<Question shortcut={mockShortcut} attempt={mockAttempt.attempt} />);
+});
 
-xit('it will call attempt(true) if the correct keys are pressed', () => {
-  let mockAttempt = {
-    attempt: function () {}
-  }
-  jest.spyOn(mockAttempt, "attempt").mockImplementation(() => '')
+const mockKeyDown = (key) => {
+  return {
+    preventDefault: function () {},
+    repeat: false,
+    key: key,
+  };
+};
 
-  const mockShortcut = { name: "Copy", combo: ['c'] }
-  
-  const wrapper = shallow(<Question shortcut={mockShortcut} attempt={mockAttempt.attempt}/>)
+const mockKeyUp = {
+  preventDefault: function () {},
+};
 
-  wrapper.find('div').simulate('keypress', {key: 'c'})
+it("correct immediately calls attempt with score 5, incorrect 0", () => {
+  const mock = {
+    attempt: function () {},
+  };
+  jest.spyOn(mock, "attempt");
 
-  expect(mockAttempt.attempt).toBeCalled();
-})
+  const mockShortcut = { name: "Copy", combo: ["c"] };
+
+  const wrapper = shallow(
+    <Question shortcut={mockShortcut} attempt={mock.attempt} />
+  );
+  const instance = wrapper.instance();
+
+  instance.keyDown(mockKeyDown("c"));
+  instance.keyDown(mockKeyUp);
+
+  expect(mock.attempt).toBeCalledWith(5, 0);
+});
+
