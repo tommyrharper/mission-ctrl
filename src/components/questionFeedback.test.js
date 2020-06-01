0e206b9c2fd52570b9908d8b87946e16dd5c0776
Harry Turnbull <hturnbull@live.co.uk>
2020-06-01 14:54:09 +0100

    adjust to h3, and context

--- src/components/questionFeedback.test.js
+++ src/components/questionFeedback.test.js
@@ -10,6 +10,6 @@ it("renders without crashing, taking a shortcut combo as prop", () => {
 it("displays the passed combo", () => {
   const combo = ["Control", "c"]
   const wrapper = shallow(<QuestionFeedback combo={combo} />);
-  const phrase = <h2>Control + c</h2>
+  const phrase = <h3>Try this: Control + c</h3>
   expect(wrapper).toContainReact(phrase);
 });
