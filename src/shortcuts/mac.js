89a65803e4ae077e56c8f239b267cf09bd9961d2
Harry Turnbull <hturnbull@live.co.uk>
2020-06-02 12:23:46 +0100

    add mac and windows shortcut sets

---
+++ src/shortcuts/mac.js
@@ -0,0 +1,8 @@
+const macShortcuts = [
+  { name: "Copy", combo: ['Meta', 'c'] },
+  { name: "Cut", combo: ['Meta', 'x'] },
+  { name: "Undo", combo: ['Meta', 'z'] },
+  { name: "Paste", combo: ['Meta', 'v'] }
+]
+
+export default macShortcuts
---
+++ src/shortcuts/windows.js
@@ -0,0 +1,8 @@
+const windowsShortcuts = [
+  { name: "Copy", combo: ['Control', 'c'] },
+  { name: "Cut", combo: ['Control', 'x'] },
+  { name: "Undo", combo: ['Control', 'z'] },
+  { name: "Paste", combo: ['Control', 'v'] }
+]
+
+export default windowsShortcuts
