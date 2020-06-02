68c6d81d557025d42b165aed1c458c7c128c8125
Alex Lewis <alexlewis374@gmail.com>
2020-06-02 12:47:02 +0100

    Timer counts down in centiseconds

--- src/components/timer.js
+++ src/components/timer.js
@@ -4,26 +4,38 @@ export class Timer extends Component {
   constructor(props){
     super(props)
     this.state = {
-      seconds: 5
+      seconds: 5,
+      centiseconds: 500
     }
   }
 
   componentDidMount () {
+    this.centisecondInterval = setInterval(() => {
+      if (this.state.centiseconds > 0) {
+        this.setState(prevState => ({
+          centiseconds: prevState.centiseconds - 1
+        }))
+      }
+    }, 10)
+    if (this.state.seconds === 0) {
+      clearInterval(this.centisecondInterval)
+    }
     this.secInterval = setInterval(() => {
       if (this.state.seconds > 0) {
         this.setState(prevState => ({
-          seconds: prevState.seconds -1
+          seconds: prevState.seconds -1,
         }))
       } 
       if (this.state.seconds === 0) {
         clearInterval(this.secInterval)
-        this.props.complete()
+        // this.props.complete()
       }
     },1000)
+    
   } 
 
   render() {
-    const seconds = <p>Time left: {this.state.seconds}</p>
+    const seconds = <p>Time left: {this.state.seconds}:{this.state.centiseconds < 10 ? `0${ this.state.centiseconds}` : this.state.centiseconds}</p>
     return (
       <div>
         {seconds}
