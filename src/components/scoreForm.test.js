ed27e980a01c5eecd6b6de5c4d4e04c8392ddee1
Alexa <alexakearns@outlook.com>
2020-06-02 11:11:45 +0100

    remove score form

--- src/components/scoreForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ScoreForm from './ScoreForm.js'
-import { shallow } from 'enzyme'
-
-describe('scoreForm', () => {
-  it('renders without crashing', () => {
-    shallow(<ScoreForm/>)
-  })
-})
