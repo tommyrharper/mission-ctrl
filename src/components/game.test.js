bd3842483cd7b52c9914339b92c73818cd40c976
Harry Turnbull <hturnbull@live.co.uk>
2020-06-02 15:04:47 +0100

    Test: Game in progress displays score

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Game from './game.js'
-import { shallow } from 'enzyme'
-import macShortcuts from '../shortcuts/mac'
+import React from "react";
+import Game from "./game.js";
+import { shallow } from "enzyme";
+import macShortcuts from "../shortcuts/mac";
+import Question from "./question.js";
 
-it('renders without crashing', () => {
-  shallow(<Game shortcuts={macShortcuts}/>)
-})
+it("renders without crashing, taking shortcuts as prop", () => {
+  shallow(<Game shortcuts={macShortcuts} />);
+});
 
-it('it displays the score', () => {
-  const wrapper = shallow(<Game shortcuts={macShortcuts}/>)
-  const score = <p>Score: 0</p>
-  expect(wrapper).toContainReact(score)
-})
+describe("Game in progress", () => {
+  it("displays the score, which can increment", () => {
+    const wrapper = shallow(<Game shortcuts={macShortcuts} />);
+    const instance = wrapper.instance();
+
+    const score0 = <p>Score: 0</p>;
+    expect(wrapper).toContainReact(score0);
+
+    instance.questionComplete(5, 0);
+
+    const score5 = <p>Score: 5</p>;
+    expect(wrapper).toContainReact(score5);
+  });
+
+});
