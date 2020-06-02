4a8be669e816b4be900d29224bb424995bc3700c
Harry Turnbull <hturnbull@live.co.uk>
2020-06-02 15:20:25 +0100

    Test: Game when complete has a try again button

--- src/components/game.js
+++ src/components/game.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import Question from './question'
-import ScoreSummary from './scoreSummary'
-import Scoreboard from './scoreboard'
+import React, { Component } from "react";
+import Question from "./question";
+import ScoreSummary from "./scoreSummary";
+import Scoreboard from "./scoreboard";
 
 export class Game extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.initialState = {
       totalIncorrect: 0,
       totalCorrect: 0,
       currentShortcut: 0,
       gameComplete: false,
       gameLength: 5000,
-      score: 0
-    }
-    this.state = this.initialState
+      score: 0,
+    };
+    this.state = this.initialState;
   }
 
   questionComplete = (score, incorrectAttempts) => {
@@ -22,61 +22,64 @@ export class Game extends Component {
       totalCorrect: this.state.totalCorrect + 1,
       totalIncorrect: this.state.totalIncorrect + incorrectAttempts,
       score: this.state.score + score,
-      currentShortcut: this.randomShortcut()
-    })
-  }
+      currentShortcut: this.randomShortcut(),
+    });
+  };
 
   completeGame = () => {
-    this.setState({gameComplete: true})
-  }
+    this.setState({ gameComplete: true });
+  };
 
   tryAgain = () => {
-    this.setState(this.initialState)
-  }
+    this.setState(this.initialState);
+  };
 
   render() {
-    let tryAgain = <button onClick={this.tryAgain}>Try Again</button>
-    if (this.props.gameComplete && !this.state.formSent) {
-      return(
+    let tryAgain = <button onClick={this.tryAgain}>Try Again</button>;
+    if (this.state.gameComplete) {
+      return (
         <div>
           {tryAgain}
-          <ScoreSummary 
-          score={this.state.score} 
-          totalCorrect={this.state.totalCorrect} 
-          totalIncorrect={this.state.totalIncorrect} 
-          gameLength={this.state.gameLength}/>
+          <ScoreSummary
+            score={this.state.score}
+            totalCorrect={this.state.totalCorrect}
+            totalIncorrect={this.state.totalIncorrect}
+            gameLength={this.state.gameLength}
+          />
           {/* <ScoreForm score={this.state.score}/> */}
         </div>
-      )
+      );
     } else if (this.state.formSent) {
       return (
-      <div>
-        {tryAgain}
-        <ScoreSummary 
-        score={this.state.score} 
-        totalCorrect={this.state.totalCorrect} 
-        totalIncorrect={this.state.totalIncorrect} 
-        gameLength={this.state.gameLength}/>
-        <Scoreboard />
-      </div>
-      )
+        <div>
+          {tryAgain}
+          <ScoreSummary
+            score={this.state.score}
+            totalCorrect={this.state.totalCorrect}
+            totalIncorrect={this.state.totalIncorrect}
+            gameLength={this.state.gameLength}
+          />
+          <Scoreboard />
+        </div>
+      );
     } else {
-      return(
+      return (
         <div>
           {/* <Timer gameLength={this.state.gameLength} complete={this.completeGame}/> */}
           <p>Score: {this.state.score}</p>
-          <Question 
-          shortcut={this.props.shortcuts[this.state.currentShortcut]}
-          attempt={this.questionComplete}/>
+          <Question
+            shortcut={this.props.shortcuts[this.state.currentShortcut]}
+            attempt={this.questionComplete}
+          />
         </div>
-      )
+      );
     }
   }
 
   randomShortcut = () => {
-    const shortcuts = this.props.shortcuts
-    return Math.floor(Math.random() * shortcuts.length)
-  }
+    const shortcuts = this.props.shortcuts;
+    return Math.floor(Math.random() * shortcuts.length);
+  };
 }
 
-export default Game
+export default Game;
