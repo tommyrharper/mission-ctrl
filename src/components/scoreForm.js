4e16b98765368d16b456eef5d7de15ead34d3319
Graham <grahamalconer@icloud.com>
2020-06-02 12:32:21 +0100

    added button to scoreform

--- src/components/scoreForm.js
+++ src/components/scoreForm.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class scoreForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+    }
+  }
+  formatScore = () => {
+    const formattedScore = {  }
+
+  }
   render() {
     return (
       <div>
         <code>Enter your name: </code>
         <input type="text"/>
+        <button onClick={this.submitScore}>Submit Score</button>
       </div>
     )
   }
--- src/components/scoreForm.test.js
+++ src/components/scoreForm.test.js
@@ -7,15 +7,43 @@ describe('scoreForm', () => {
     shallow(<ScoreForm/>)
   })
 
+  xit('shows your score', () => {
+
+  })
+
   it('prompts the user to enter their name', () => {
     const wrapper = shallow(<ScoreForm/>)
     const namePrompt = <code>Enter your name: </code>
     expect(wrapper).toContainReact(namePrompt)
   })
 
-  it('allows user to enter name', () => {
+  it('has input for user to enter name', () => {
     const wrapper = shallow(<ScoreForm/>)
     const inputBox = <input type="text"/>
     expect(wrapper).toContainReact(inputBox)
+  })
+
+  describe('button, when clicked...', () => {
+    // it('creates object to submit with name and score', () => {
+      // const wrapper = shallow(<ScoreForm score={15}/>)
+      // const mockSubmission = { name: "John", score: }
+      // // formatScore = function (){
+      //   let emptyObject = {}
+      //   emptyObject.name = name from form
+      //   emptyObject.score = score from game
+      // }
+      // // --->
+      // expectedOutput{ name: "John", score: 15 }
+
+      // expect(formatScore).toEq(expectedOutput)
+
+    })
+    xit('makes a post request to the database', () => {
+
+    })
+
+    xit('brings you back to start game', () => {
+
+    })
   })
 })
