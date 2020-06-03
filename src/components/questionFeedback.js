951bb30c237631a6e93747e3b5ad572131625e41
Alex Lewis <alexlewis374@gmail.com>
2020-06-03 17:18:31 +0100

    G: Gives a hint for a combo that is three long

--- src/components/questionFeedback.js
+++ src/components/questionFeedback.js
@@ -7,7 +7,17 @@ export class QuestionFeedback extends Component {
     return this.props.combo.join(" + ");
   };
 
+  firstKey = () => {
+    return this.props.combo[0]
+  }
+
   render() {
+    if (this.props.incorrectAttempts === 2 && this.props.combo.length === 3) {
+      return <p>Hint: {this.firstKey()} + ? + ?</p>
+    } 
+    if (this.props.incorrectAttempts === 2) {
+      return <p>Hint: {this.firstKey()} + ?</p>
+    }
     if (this.props.incorrectAttempts > ANSWER_THRESHOLD) {
       return <h3>Try this: {this.joinCombo()}</h3>;
     }
