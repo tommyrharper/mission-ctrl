a06b802cb73ec2f4d8d07922bfa6e0a663a60042
Alexa <alexakearns@outlook.com>
2020-06-03 17:32:58 +0100

    mac commands up to 33

--- src/shortcuts/mac.js
+++ src/shortcuts/mac.js
@@ -1,8 +1,54 @@
 const macShortcuts = [
-  { name: "Copy", combo: ['Meta', 'c'] },
-  { name: "Cut", combo: ['Meta', 'x'] },
-  { name: "Undo", combo: ['Meta', 'z'] },
-  { name: "Paste", combo: ['Meta', 'v'] }
+  { name: "Show Command Palette", combo: ['Shift', 'Meta', 'p'] },
+  { name: "Quick Open, Go to File..", combo: ['Meta', 'p'] },
+  { name: "User Settings", combo: ['Meta', ','] },
+  { name: "Cut Line (empty selection)", combo: ['Meta', 'x'] },
+  { name: "Copy Line (empty selection)", combo: ['Meta', 'c'] },
+  { name: "Move line up", combo: ['Alt', 'ArrowUp'] },
+  { name: "Move line down", combo: ['Alt', 'ArrowDown'] },
+  { name: "Copy line up", combo: ['Shift', 'Alt', 'ArrowUp'] },
+  { name: "Copy line down", combo: ['Shift', 'Alt', 'ArrowDown' ] },
+  { name: "Delete line", combo: ['Shift', 'Meta', 'k'] },
+  { name: "Insert line above", combo: ['Meta', 'Enter'] },
+  { name: "Insert line below", combo: ['Shift', 'Meta', 'Enter'] },
+  { name: "Jump to matching bracket", combo: ['Shift', 'Meta', '\\'] },
+  { name: "Indent line", combo: ['Meta', ']'] },
+  { name: "Outdent line", combo: ['Meta', '['] },
+  { name: "Go to beginning of line", combo: ['Meta', 'ArrowUp'] },
+  { name: "Go to end of line", combo: ['Meta', 'ArrowDown'] },
+  { name: "Toggle line comment", combo: ['Meta', '/'] },
+  { name: "Insert cursor above", combo: ['Alt', 'Shift', 'ArrowUp'] },
+  { name: "Insert cursor below", combo: ['Alt', 'Shift', 'ArrowDown'] },
+  { name: "Find", combo: ['Meta', 'f'] },
+  { name: "Find next", combo: ['Meta', 'g'] },
+  { name: "Find previous", combo: ['Shift', 'Meta', 'g'] },
+  { name: "Open File..", combo: ['Meta', 'o'] },
+  { name: "Save", combo: ['Meta', 's'] },
+  { name: "Reopen closed editor", combo: ['Shift', 'Meta', 't'] },
+  { name: "Show integrated terminal", combo: ['Control', '`'] },
+  { name: "Toggle full screen", combo: ['Meta', 'f'] },
+  { name: "Create new terminal", combo: ['Control', 'Shift', '`'] },
+  { name: "Zoom in", combo: ['Meta', '='] },
+  { name: "Zoom out", combo: ['Shift', 'Meta', '-'] },
+  { name: "Expand selection", combo: ['Control', 'Shift', 'Meta', 'ArrowLeft'] },
+  { name: "Shrink selection", combo: ['Control', 'Shift', 'Meta', 'ArrowRight'] },
+  { name: "Toggle Sidebar visability", combo: ['Meta', 'b']}
 ]
+
+// { name: "New window/instance", combo: ['Shift', 'Meta', 'n'] },
+// { name: "Replace", combo: ['Alt', 'Meta', 'f'] },
+// { name: "Format document", combo: ['Shift', 'Alt', 'f'] },
+// { name: "Close editor", combo: ['Meta', 'w'] },
+// { name: "Save All", combo: ['Alt', 'Meta', 's'] },///////////////////////////////////////////////////
+// Opens new tab - can't override?
+// { name: "New File", combo: ['Meta', 'n'] },
+// { name: "Save As..", combo: ['Shift', 'Meta', 's'] },
+
+// [
+//   { name: "Copy", combo: ['Meta', 'c'] },
+//   { name: "Cut", combo: ['Meta', 'x'] },
+//   { name: "Undo", combo: ['Meta', 'z'] },
+//   { name: "Paste", combo: ['Meta', 'v'] }
+// ]
 
 export default macShortcuts
