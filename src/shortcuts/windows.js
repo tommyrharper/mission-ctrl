956caef6c4ed7b8848726ba781621d36bde503c3
Alexa <alexakearns@outlook.com>
2020-06-03 17:39:39 +0100

    windows file contains mac commands

--- src/shortcuts/windows.js
+++ src/shortcuts/windows.js
@@ -1,8 +1,43 @@
 const windowsShortcuts = [
-  { name: "Copy", combo: ['Control', 'c'] },
-  { name: "Cut", combo: ['Control', 'x'] },
-  { name: "Undo", combo: ['Control', 'z'] },
-  { name: "Paste", combo: ['Control', 'v'] }
+  { name: "Show Command Palette", combo: ['Shift', 'Meta', 'p'] },
+  { name: "Quick Open, Go to File..", combo: ['Meta', 'p'] },
+  { name: "User Settings", combo: ['Meta', ','] },
+  { name: "Cut Line (empty selection)", combo: ['Meta', 'x'] },
+  { name: "Copy Line (empty selection)", combo: ['Meta', 'c'] },
+  { name: "Move line up", combo: ['Alt', 'ArrowUp'] },
+  { name: "Move line down", combo: ['Alt', 'ArrowDown'] },
+  { name: "Copy line up", combo: ['Shift', 'Alt', 'ArrowUp'] },
+  { name: "Copy line down", combo: ['Shift', 'Alt', 'ArrowDown' ] },
+  { name: "Delete line", combo: ['Shift', 'Meta', 'k'] },
+  { name: "Insert line above", combo: ['Meta', 'Enter'] },
+  { name: "Insert line below", combo: ['Shift', 'Meta', 'Enter'] },
+  { name: "Jump to matching bracket", combo: ['Shift', 'Meta', '\\'] },
+  { name: "Indent line", combo: ['Meta', ']'] },
+  { name: "Outdent line", combo: ['Meta', '['] },
+  { name: "Go to beginning of line", combo: ['Meta', 'ArrowUp'] },
+  { name: "Go to end of line", combo: ['Meta', 'ArrowDown'] },
+  { name: "Toggle line comment", combo: ['Meta', '/'] },
+  { name: "Insert cursor above", combo: ['Alt', 'Shift', 'ArrowUp'] },
+  { name: "Insert cursor below", combo: ['Alt', 'Shift', 'ArrowDown'] },
+  { name: "Find", combo: ['Meta', 'f'] },
+  { name: "Find next", combo: ['Meta', 'g'] },
+  { name: "Find previous", combo: ['Shift', 'Meta', 'g'] },
+  { name: "Open File..", combo: ['Meta', 'o'] },
+  { name: "Save", combo: ['Meta', 's'] },
+  { name: "Reopen closed editor", combo: ['Shift', 'Meta', 't'] },
+  { name: "Show integrated terminal", combo: ['Control', '`'] },
+  { name: "Toggle full screen", combo: ['Meta', 'f'] },
+  { name: "Create new terminal", combo: ['Control', 'Shift', '`'] },
+  { name: "Zoom in", combo: ['Meta', '='] },
+  { name: "Zoom out", combo: ['Shift', 'Meta', '-'] },
+  { name: "Expand selection", combo: ['Control', 'Shift', 'Meta', 'ArrowLeft'] },
+  { name: "Shrink selection", combo: ['Control', 'Shift', 'Meta', 'ArrowRight'] },
+  { name: "Toggle Sidebar visability", combo: ['Meta', 'b']}
 ]
 
 export default windowsShortcuts
+
+// { name: "Copy", combo: ['Control', 'c'] },
+// { name: "Cut", combo: ['Control', 'x'] },
+// { name: "Undo", combo: ['Control', 'z'] },
+// { name: "Paste", combo: ['Control', 'v'] }
