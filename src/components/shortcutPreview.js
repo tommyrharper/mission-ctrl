d7386a6606996a567bd5cffbd1351a73945f84e7
Harry Turnbull <hturnbull@live.co.uk>
2020-06-04 15:04:28 +0100

    spike shortcut preview

---
+++ src/components/shortcutPreview.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class ShortcutPreview extends Component {
+  joinCombo = (combo) => {
+    return combo.join(" + ")
+  }
+
+  shortcutRow = (shortcut) => {
+    return (
+      <tr key={shortcut.name} className="score">
+        <td>{shortcut.name}.</td>
+        <td>{this.joinCombo(shortcut.hint)}</td>
+      </tr>
+    );
+  };
+
+  render() {
+    const shortcutsMapped = this.props.shortcuts.map(this.shortcutRow);
+
+    return (
+      <div>
+        <h2>Shortcut List</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Shortcut</th>
+              <th>Key Combo</th>
+            </tr>
+          </thead>
+          <tbody>{shortcutsMapped}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default ShortcutPreview;
